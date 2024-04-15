a1fa930010906339333f8654f3e23f9b2e484c1c
X0rb1t <who4mi.s3curity@outlook.com>
2024-04-15 00:58:28 -0300

    Estruturando API REST

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { server } from './server/Server';
+import { server } from './bin/Server';
 
 server.listen(process.env.PORT || 3333, () => {
     console.log(`Server is running on ${process.env.PORT || 3333}`);
